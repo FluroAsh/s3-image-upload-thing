f7eda705bf00e0c84f60180cec0a6d30cb67d27a
Ashley Thompson <workashleythompson@gmail.com>
2025-01-07 15:59:22 +1100

    chore: Adds react icons + clsx/twM package for handling TW classes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
+  // TODO: Add an error boundary for this layout/route
   return <div className="p-4">{children}</div>
 }
