11486831075856b099b07ad0c7f38df55f80f876
Ashley Thompson <workashleythompson@gmail.com>
2025-01-07 15:58:08 +1100

    chore: Update env variables so we can use NEXT_PUBLIC variable on client-side

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const axiosInstance = axios.create({
   baseURL: process.env.API_URL || 'http://localhost:3002',
   headers: {
-    'x-amz-bucket-region': process.env.DEFAULT_REGION || 'ap-southeast-2'
+    'x-amz-bucket-region': process.env.NEXT_PUBLIC_S3_REGION || 'ap-southeast-2'
   }
 })
 
