1bcdf18500a65d7cbd04fb762a53df288237779b
Ashley Thompson <workashleythompson@gmail.com>
2025-01-07 15:58:48 +1100

    feat: Adds page markup with new Explorer component + URL param logic for handling bucket state

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,30 +1,31 @@
-import BucketDisplay from '@/components/bucket-display'
-import S3BucketCard from '@/components/s3-card'
-import { getBuckets } from '@/services/s3'
+import { BucketList } from '@/components/bucket-list'
+import { getBuckets, getFileTree } from '@/services/s3'
+import { Explorer, ViewPanel, ActivePanel } from '@/components/explorer'
 
-export default async function Page() {
+export default async function Page({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) {
   const buckets = await getBuckets()
 
+  const activeBucket = ((await searchParams)?.bucket as string | undefined) || buckets[0]?.Name
+
+  // initial fetch should be URL param fetch or first bucket (fallback to first buck if the request fails - no res.ok)
+
+  const initialTree = await getFileTree(activeBucket ?? buckets[0]?.Name)
+
   return (
-    <div className="grid grid-rows-2">
+    <div className="grid grid-rows-[auto_fill_1fr]">
       <div className="mb-8 overflow-auto">
         <p className="text-xl font-bold pb-2">Bucket List</p>
 
         {/* Cards should scroll when overflowing the container (max-screen-width) */}
-        <div className="flex overflow-x-auto gap-4">
-          {buckets.map((bucket, idx: number) => (
-            <S3BucketCard
-              key={idx}
-              name={bucket.Name}
-              region={bucket.BucketRegion}
-              formattedCreationDate={bucket.formattedCreationDate}
-            />
-          ))}
-        </div>
+        <BucketList buckets={buckets} />
       </div>
 
       {/* TODO: Finish bucket display component */}
-      <BucketDisplay />
+      {/* Pass the tree to the root component */}
+      <Explorer bucketName={activeBucket}>
+        <ViewPanel fileTree={initialTree} bucketName={activeBucket} />
+        <ActivePanel />
+      </Explorer>
     </div>
   )
 }
