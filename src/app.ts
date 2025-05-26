5a348af609e163794b42d6595c50c8db30c0e6a3
Ashley Thompson <workashleythompson@gmail.com>
2025-05-26 18:03:07 +1000

    refactor: Updates packages & increase maximum request body size

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,22 @@
 
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { bodyLimit } from 'hono/body-limit'
+
 import s3 from './features/s3/route'
 import image from './features/image/route'
 
 const app = new Hono()
+const maxSize = Number.MAX_SAFE_INTEGER // Virtually unlimited size (DO NOT use this in Prod)
 
-app.use('*', cors())
+app.use(
+  '*',
+  cors(),
+  bodyLimit({
+    maxSize,
+    onError: (c) => c.json({ error: 'Request body too large' }, 413)
+  })
+)
 
 app.route('/s3', s3)
 app.route('/image', image)
@@ -27,5 +37,6 @@ console.log('Server started on port 3002')
 
 export default {
   port: process.env.PORT || 3002,
-  fetch: app.fetch
+  fetch: app.fetch,
+  maxRequestBodySize: maxSize
 }
