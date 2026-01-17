08bfade2e74e75efa536bb15cbc7e1466c577be2
Ashley Thompson <workashleythompson@gmail.com>
2026-01-17 23:15:58 +1100

    chore: Comment update

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    // Required for Production builds to proxy /proxy/* to the Docker-internal API service
+    // Required for Production builds to proxy /proxy/* requests to the Docker-internal API service
     // This will always be executed on the server, so we don't need a "client" safe URL with an actual hostname
     const apiUrl = process.env.API_URL || 'http://localhost:3002';
     return [
