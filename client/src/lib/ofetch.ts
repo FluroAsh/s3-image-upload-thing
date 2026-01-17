77dbd652aa585fb9d3e1edfa5aca6b86865e7175
Ashley Thompson <workashleythompson@gmail.com>
2026-01-17 20:45:04 +1100

    fix: Updates API_URL -> NEXT_PUBLIC_API_URL so it is accessible on the client

--- client/src/lib/ofetch.ts
+++ client/src/lib/ofetch.ts
@@ -1,7 +1,7 @@
 import { ofetch } from 'ofetch'
 
 const ofetchInstance = ofetch.create({
-  baseURL: process.env.API_URL || 'http://localhost:3002',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3002',
   headers: {
     'x-amz-bucket-region': process.env.NEXT_PUBLIC_S3_REGION || 'ap-southeast-2'
   }
